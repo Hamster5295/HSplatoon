270372cb3926d1b81dff5e98d28f9e0e7ffc28f5
Hamster5295 <hamster5295@163.com>
2022-10-02 00:22:19 +0800

    完善Weapon类的接口

--- Script/Weapon/Weapon.cs
+++ Script/Weapon/Weapon.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 
 public class Weapon : Component<Unit>
 {
+    [Signal] public delegate void OnUseBegin();
+    [Signal] public delegate void OnUseStay(float delta);
+    [Signal] public delegate void OnUseEnd();
+
     [Export] public PackedScene bullet;
+    [Export] public WeaponType type;
     [Export] public float damage;
 
     private Sprite sprite;
     private Tween tween;
 
     private List<Position2D> heads = new List<Position2D>();
+
+    public List<Position2D> Heads { get => heads; private set => heads = value; }
 
     public override void _Ready()
     {
@@ -18,7 +25,17 @@ public class Weapon : Component<Unit>
 
         foreach (var item in GetNode<Node2D>("Heads").GetChildren())
         {
-            if (item is Position2D p) heads.Add(p);
+            if (item is Position2D p) Heads.Add(p);
         }
     }
+
+    public Vector2 GetDirection()
+    {
+        return Vector2.Up.Rotated(GlobalRotation);
+    }
+}
+
+public enum WeaponType
+{
+    Gun, Bucket, Brush
 }
